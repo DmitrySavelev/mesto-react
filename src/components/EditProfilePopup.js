12e16f3a8ff31c68250adb32ab770906aeb8b0a1
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-01-22 04:20:43 +0700

    before the 2nd sending

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -3,18 +3,16 @@ import PopupWithForm from "./PopupWithForm";
 const EditProfilePopup = (props) => {
   return (
     <PopupWithForm title={props.title} name={props.name} isOpen={props.isOpen} onClose={props.onClose}>
-      <form name="popupForm" className={`popup__form popup__form_${props.name}`} noValidate>
-        <label className="popup__form-field">
-          <input name="inputName" type="text" id="name-input" className="popup__input popup__input_name"
-            placeholder="Ваше имя" minLength="2" maxLength="40" required />
-          <span className="popup__error name-input-error"></span>
-        </label>
-        <label className="popup__form-field">
-          <input name="inputJob" type="text" id="job-input" className="popup__input popup__input_job"
-            placeholder="Ваша специализация" minLength="2" maxLength="200" required />
-          <span className="popup__error job-input-error"></span>
-        </label>
-      </form>
+      <label className="popup__form-field">
+        <input name="inputName" type="text" id="name-input" className="popup__input popup__input_name"
+          placeholder="Ваше имя" minLength="2" maxLength="40" required />
+        <span className="popup__error name-input-error"></span>
+      </label>
+      <label className="popup__form-field">
+        <input name="inputJob" type="text" id="job-input" className="popup__input popup__input_job"
+          placeholder="Ваша специализация" minLength="2" maxLength="200" required />
+        <span className="popup__error job-input-error"></span>
+      </label>
     </PopupWithForm>
   )
 }
